22886eeafc5a8d34ec956ce9e71945f7195e4b7b
marianobazanc <marianobazancc@gmail.com>
2021-07-19 17:16:12 -0300

    Primer commit de la ramaMariano

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './pages/home/home.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { LayoutModule } from './layout/layout.module';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +18,9 @@ import { LayoutModule } from './layout/layout.module';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    LayoutModule
+    LayoutModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {Validators, FormGroup, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
+
 export class RegistroComponent implements OnInit {
+  
+  
+  signupForm: FormGroup;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.signupForm = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      dni: ['', Validators.required],
+      usuario: ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      clave: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+      mail: ['', Validators.compose([Validators.required, Validators.email])],
+    })
   }
 
+  enviar(values) {
+    console.log(values)
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  
 }
