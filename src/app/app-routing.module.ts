30b444cccae242a38a41d5e9860a3c4c3816d374
MariaAngelaLuzAltamirano <marian_1490_1@hotmail.com>
2021-07-19 02:09:11 -0300

    actualizando navBar, Footer y creando pág de Inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 //importar modulos
 import { HomeComponent } from './pages/home/home.component';
 import { RegistroComponent } from './pages/registro/registro.component';
+import { PagBienvenidaComponent } from './pages/pag-bienvenida/pag-bienvenida.component';
 
 const routes: Routes = [
   { path: 'login', component: HomeComponent },
   { path: 'registro', component: RegistroComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'bienvenida' , component: PagBienvenidaComponent},
 
-  { path: '', component: HomeComponent },
-  { path: '**', component: HomeComponent } //Not found 404
+  { path: '', component: PagBienvenidaComponent },
+  { path: '**', component: PagBienvenidaComponent } //Not found 404
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './pages/home/home.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { LayoutModule } from './layout/layout.module';
+import { PagBienvenidaComponent } from './pages/pag-bienvenida/pag-bienvenida.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    RegistroComponent
+    RegistroComponent,
+    PagBienvenidaComponent
   ],
   imports: [
     BrowserModule,
