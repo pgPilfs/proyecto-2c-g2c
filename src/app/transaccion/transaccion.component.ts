d9f9244a02f2e69ba529b77509a6986a61c81d99
marianobazanc <marianobazancc@gmail.com>
2021-07-27 12:09:13 -0300

    Se añade la validacion de que el monto a transferir sea menor o igual al saldo disponible

--- src/app/transaccion/transaccion.component.ts
+++ src/app/transaccion/transaccion.component.ts
@@ -9,23 +9,35 @@ import { Validators, FormGroup, FormBuilder} from '@angular/forms';
 export class TransaccionComponent implements OnInit {
 
   transferForm: FormGroup;
+  saldoDisponible: number;
+  excedeSaldo: boolean;
+  textoIncorrecto: string;
 
   constructor(private formBuilder: FormBuilder) { 
+    this.saldoDisponible = 90000;
+    this.textoIncorrecto = '';
     this.transferForm = this.formBuilder.group({
       nombre: ['', Validators.compose([Validators.required, Validators.minLength(4)])],
       dni: ['', Validators.compose([Validators.required, Validators.min(1000)])],
       mail: ['', Validators.compose([Validators.required, Validators.email])],
       monto: ['', Validators.compose([Validators.min(0), Validators.required, Validators.max(400000)])],
       cbu: ['', Validators.compose([Validators.required, Validators.min(1000)])],
-      motivo: [''],
-      saldoDisponible: ['20000']
-      
-
+      motivo: ['']
     })
    }
 
   enviar(values) {
     console.log(values)
+  }
+
+  validarCantidad() {
+    if(this.transferForm.get("monto").value>this.saldoDisponible){
+      this.excedeSaldo = true;
+      this.textoIncorrecto = 'El monto a transferir es mayor al saldo disponible'
+      return;
+    } else {
+      this.excedeSaldo = false;
+    }
   }
 
   ngOnInit(): void {
