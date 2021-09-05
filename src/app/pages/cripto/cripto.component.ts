20e6f6ad1e1a4ec016dfb3f4df81a111e194de27
Mariela Britos <ally_mab@hotmail.com>
2021-09-05 12:57:59 -0300

    criptomonedas

--- src/app/pages/cripto/cripto.component.ts
+++ src/app/pages/cripto/cripto.component.ts
@@ -1,18 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
+
+@Component({
+  selector: 'ngbd-modal-content',
+  template: `
+    <div class="modal-header">
+      <h4 class="modal-title">Usted va a realizar una compra!</h4>
+      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body">
+    <input class="form-control" type="text" placeholder="Monto a comprar:">
+    
+      <p>Hello, {{name}}!</p>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Aceptar</button>
+    </div>
+  `
+})
+export class NgbdModalContent {
+  @Input() name;
+
+  constructor(public activeModal: NgbActiveModal) {}
+}
 
 @Component({
   selector: 'app-cripto',
   templateUrl: './cripto.component.html',
+  
   styleUrls: ['./cripto.component.css']
 })
-export class CriptoComponent implements OnInit {
+export class CriptoComponent {
+  @Input() name;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private modalService: NgbModal) {}
+  open() {
+    const modalRef = this.modalService.open(NgbdModalContent);
+    modalRef.componentInstance.name = 'World';
   }
-clickAddTodo() {
-  alert('Compra exitosa!');
+
 }
-}
+
