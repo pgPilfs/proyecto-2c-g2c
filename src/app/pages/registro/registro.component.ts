160fafde0f446c3b3306d303471ccefc31b958f5
Mariela Britos <ally_mab@hotmail.com>
2021-09-15 18:03:50 -0300

    modificacion registro

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Validators, FormGroup, FormBuilder} from '@angular/forms';
 import { usuarios, Usuario } from '../../models/usuario';
 import { RegistroService } from '../../servicios/registro.service';
+
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
@@ -19,12 +20,14 @@ export class RegistroComponent implements OnInit {
       nombre: ['', Validators.required],
       apellido: ['', Validators.required],
       dni: ['', Validators.compose([Validators.required])],
-      usuario: ['', Validators.compose([Validators.required, Validators.minLength(4)])],
-      clave: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
-      mail: ['', Validators.compose([Validators.required, Validators.email])],
+      
+      clave: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+      clave2: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
     })
   }
 
+  
   enviar(values: Usuario) {
     this.serv.registro(values);
   }
